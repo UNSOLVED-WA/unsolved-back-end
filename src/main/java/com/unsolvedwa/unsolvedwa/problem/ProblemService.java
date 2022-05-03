ae2238f56610f193b10fc512757b46061b6c3a32
hainho <hainho9704@gmail.com>
2022-05-03 19:21:42 +0900

    ♻️ refactor : Changing dependency injection from field injection to constructor injection

--- src/main/java/com/unsolvedwa/unsolvedwa/problem/ProblemService.java
+++ src/main/java/com/unsolvedwa/unsolvedwa/problem/ProblemService.java
@@ -1,14 +1,17 @@
 package com.unsolvedwa.unsolvedwa.problem;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.Repository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service("problemService")
+@RequiredArgsConstructor
 public class ProblemService {
-	@Autowired
-	ProblemRepository problemRepository;
+
+	private final ProblemRepository problemRepository;
 
 	Problem findByProblemId(String id) {
 		return problemRepository.findById(id).get();
