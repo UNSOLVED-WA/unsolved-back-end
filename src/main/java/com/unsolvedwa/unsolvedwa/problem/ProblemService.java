b1773ffebe18521643a739ace0daa8d69c50f00e
hainho <hainho9704@gmail.com>
2022-05-03 20:17:27 +0900

    ♻️ refactor : Apply transaction

--- src/main/java/com/unsolvedwa/unsolvedwa/problem/ProblemService.java
+++ src/main/java/com/unsolvedwa/unsolvedwa/problem/ProblemService.java
@@ -8,11 +8,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service("problemService")
+@Transactional
 @RequiredArgsConstructor
 public class ProblemService {
 
 	private final ProblemRepository problemRepository;
 
+	@Transactional(readOnly = true)
 	Problem findByProblemId(String id) {
 		return problemRepository.findById(id).get();
 	}
