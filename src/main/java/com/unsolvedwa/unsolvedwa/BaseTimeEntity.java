e9a68b94515f73ba3ee1ee63ae3ada648d554e3b
siontama <siontama@naver.com>
2022-05-10 13:54:05 +0900

    :zap: Feat: createAt 변경 불가, null 불가

--- src/main/java/com/unsolvedwa/unsolvedwa/BaseTimeEntity.java
+++ src/main/java/com/unsolvedwa/unsolvedwa/BaseTimeEntity.java
@@ -5,6 +5,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
@@ -13,9 +14,9 @@ import java.time.LocalDateTime;
 @MappedSuperclass //공통맵핑정보
 @EntityListeners(AuditingEntityListener.class) //entity를감시?
 public abstract class BaseTimeEntity {
-
     @CreatedDate
-    private LocalDateTime createdAt;
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createAt;
 
     @LastModifiedDate
     private LocalDateTime updatedAt;
