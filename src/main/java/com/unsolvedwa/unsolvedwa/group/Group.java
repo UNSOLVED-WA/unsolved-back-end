d863ac066c41db9b5365a4d2b5c95c784179b52c
hainho <hainho9704@gmail.com>
2022-05-10 16:05:34 +0900

    ✨ Feat: Group entity field 추가

--- src/main/java/com/unsolvedwa/unsolvedwa/group/Group.java
+++ src/main/java/com/unsolvedwa/unsolvedwa/group/Group.java
@@ -1,12 +1,11 @@
 package com.unsolvedwa.unsolvedwa.group;
 
 import com.unsolvedwa.unsolvedwa.BaseTimeEntity;
+import com.unsolvedwa.unsolvedwa.ranking.Ranking;
 import lombok.Getter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Getter
@@ -16,4 +15,13 @@ public class Group extends BaseTimeEntity {
     private Long id;
 
     private String name;
+
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL)
+    private List<UserGroup> userGroups;
+
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL)
+    private List<ProblemGroup> problemGroups;
+
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL)
+    private List<Ranking> rankings;
 }
