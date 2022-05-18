820de8a373156ebcdb45a7f72537078e43e2065c
hainho <hainho9704@gmail.com>
2022-05-19 07:33:56 +0900

    ✨ Feat: Group entity 를 Team entity 로 수정

---
+++ src/main/java/com/unsolvedwa/unsolvedwa/team/Team.java
@@ -0,0 +1,27 @@
+package com.unsolvedwa.unsolvedwa.team;
+
+import com.unsolvedwa.unsolvedwa.BaseTimeEntity;
+import com.unsolvedwa.unsolvedwa.ranking.Ranking;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+public class Team extends BaseTimeEntity {
+    @Id @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team", cascade = CascadeType.ALL)
+    private List<UserTeam> userTeams;
+
+    @OneToMany(mappedBy = "team", cascade = CascadeType.ALL)
+    private List<ProblemTeam> problemTeams;
+
+    @OneToMany(mappedBy = "team", cascade = CascadeType.ALL)
+    private List<Ranking> rankings;
+}
