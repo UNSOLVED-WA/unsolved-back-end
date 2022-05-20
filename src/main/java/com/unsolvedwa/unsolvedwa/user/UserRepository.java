ef9f92751a41240172bc4259cc06727dc1a33a13
siontama <siontama@naver.com>
2022-05-20 15:40:28 +0900

    :sparkles: Feat: customRepository 추가

--- src/main/java/com/unsolvedwa/unsolvedwa/user/UserRepository.java
+++ src/main/java/com/unsolvedwa/unsolvedwa/user/UserRepository.java
@@ -2,6 +2,5 @@ package com.unsolvedwa.unsolvedwa.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-
+public interface UserRepository extends JpaRepository<User, Long>, UserCustomRepository{
 }
