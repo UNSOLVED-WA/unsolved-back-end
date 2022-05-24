98aa6424214f46aed209c3887062200c7c8f10b6
siontama <siontama@naver.com>
2022-05-24 13:09:14 +0900

    :fire: Remove: Repository를 앞으로 빼는 naming으로 변경

--- src/main/java/com/unsolvedwa/unsolvedwa/user/UserCustomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.unsolvedwa.unsolvedwa.user;
-
-import com.unsolvedwa.unsolvedwa.user.dto.Score;
-
-public interface UserCustomRepository {
-    Score[] solvingProblem(Long user_id, Long boj_id);
-}
