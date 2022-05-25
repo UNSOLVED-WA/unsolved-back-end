d3e9cacc862347533ef84dc1eb0263f7a78365fc
siontama <siontama@naver.com>
2022-05-25 15:50:53 +0900

    :recycle: Refactor: convention에 맞게 수정

--- src/main/java/com/unsolvedwa/unsolvedwa/domain/problem_team/ProblemTeamService.java
+++ src/main/java/com/unsolvedwa/unsolvedwa/domain/problem_team/ProblemTeamService.java
@@ -7,7 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Service("problemTeamService")
+@Service("ProblemTeamService")
 @Transactional
 @RequiredArgsConstructor
 public class ProblemTeamService {
