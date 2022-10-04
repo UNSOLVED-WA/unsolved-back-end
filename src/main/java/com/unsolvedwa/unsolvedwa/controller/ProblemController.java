48738d53c8aee3cc8bc9d13111e6b3200a69a6ab
hainho <hainho9704@gmail.com>
2022-10-04 17:56:40 +0900

    ✨ feature : problem controller 안쓰는 메서드 삭제

--- src/main/java/com/unsolvedwa/unsolvedwa/controller/ProblemController.java
+++ src/main/java/com/unsolvedwa/unsolvedwa/controller/ProblemController.java
@@ -2,17 +2,19 @@ package com.unsolvedwa.unsolvedwa.controller;
 
 import com.unsolvedwa.unsolvedwa.domain.problem.Problem;
 import com.unsolvedwa.unsolvedwa.domain.problem.ProblemService;
-import com.unsolvedwa.unsolvedwa.domain.problemteam.ProblemTeamService;
-import com.unsolvedwa.unsolvedwa.domain.problemteam.dto.ScoreDto;
+import com.unsolvedwa.unsolvedwa.domain.problem.dto.ProblemResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Tag(name = "ProblemController")
@@ -21,26 +23,38 @@ import java.util.List;
 public class ProblemController {
 
   private final ProblemService problemService;
-  private final ProblemTeamService problemTeamService;
 
   @Operation(description = "문제조회")
   @GetMapping(value = "/{id}")
   public ResponseEntity<Problem> getProblem(@Parameter @PathVariable Long id) {
-    return ResponseEntity.ok(problemService.findByProblemId(id));
-  }
-
-  @Operation(description = "특정 문제 품")
-  @PostMapping(value = "/solving")
-  public ResponseEntity<List<ScoreDto>> postSolving(@Parameter Long user_id,
-      @Parameter Long boj_id) {
-    return ResponseEntity.ok(problemTeamService.solvingProblem(user_id, boj_id));
+    try
+    {
+      return ResponseEntity.ok(problemService.findByProblemId(id));
+    }
+    catch (NotFoundException e)
+    {
+      return ResponseEntity.notFound().build();
+    }
   }
 
   @Operation(description = "unsolved 리스트 조회")
-  @GetMapping(value = "/{team_id}/{tier}")
-  public ResponseEntity<List<Problem>> getUnsolvedProblems(@Parameter @PathVariable Long team_id,
+  @GetMapping(value = "/{teamId}/{tier}")
+  public ResponseEntity<List<Problem>> getUnsolvedProblems(@Parameter @PathVariable Long teamId,
       @Parameter @PathVariable Long tier) {
     //TODO: service 구현하여 작성
     return ResponseEntity.ok(new ArrayList<>());
+  }
+
+  @Operation(description = "특정 팀, 티어의 unsolved problem 리스트 조회")
+  @GetMapping(value = "/unsolved/{teamId}/{tier}")
+  public ResponseEntity<List<ProblemResponseDto>> getUnsolvedProblemsByTeamAndTier(@Parameter @PathVariable Long teamId, @Parameter @PathVariable Long tier){
+    try
+    {
+      return ResponseEntity.ok(problemService.findUnsolvedProblemsByTeamAndTier(teamId, tier));
+    }
+    catch (NotFoundException e)
+    {
+      return ResponseEntity.notFound().build();
+    }
   }
 }
