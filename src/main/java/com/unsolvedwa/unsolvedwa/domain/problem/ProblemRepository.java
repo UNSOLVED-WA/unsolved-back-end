b55ed2e1276a5b560d3b1f59a1fb40dd14f25a4e
hainho <hainho9704@gmail.com>
2022-10-04 17:26:14 +0900

    ✨ feature : get unsolved tier repository 메서드 작성

--- src/main/java/com/unsolvedwa/unsolvedwa/domain/problem/ProblemRepository.java
+++ src/main/java/com/unsolvedwa/unsolvedwa/domain/problem/ProblemRepository.java
@@ -1,7 +1,8 @@
 package com.unsolvedwa.unsolvedwa.domain.problem;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProblemRepository extends JpaRepository<Problem, Long>, ProblemRepositoryCustom {
-  public Problem findByProblemNumber(Long problemNumber);
+  public Optional<Problem> findByProblemNumber(Long problemNumber);
 }
--- src/main/java/com/unsolvedwa/unsolvedwa/domain/problem/ProblemRepositoryCustom.java
+++ src/main/java/com/unsolvedwa/unsolvedwa/domain/problem/ProblemRepositoryCustom.java
@@ -1,7 +1,8 @@
 package com.unsolvedwa.unsolvedwa.domain.problem;
 
+import com.unsolvedwa.unsolvedwa.domain.problem.dto.ProblemResponseDto;
 import java.util.List;
 
 public interface ProblemRepositoryCustom {
-
+  public List<ProblemResponseDto> findUnsolvedProblemsByTeamAndTier(Long teamId, Long tier);
 }
