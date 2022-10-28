fbf0f18826d0e9545c89fc9cb1c8033859a0c381
KwonJieun <sy.jikwon@gmail.com>
2022-10-28 09:24:16 +0900

    🐛 bugfix : Add missing service

--- src/main/java/com/unsolvedwa/unsolvedwa/controller/ProblemController.java
+++ src/main/java/com/unsolvedwa/unsolvedwa/controller/ProblemController.java
@@ -31,6 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProblemController {
 
   private final ProblemService problemService;
+  private final ProblemTeamService problemTeamService;
 
   @Operation(description = "문제조회")
   @GetMapping(value = "/{id}")
