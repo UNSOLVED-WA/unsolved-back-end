1c6de5bb0d9d22a7898a59c3ff0ddf9c36b73996
hainho <hainho9704@gmail.com>
2022-11-16 00:03:10 +0900

    ✨ feature : 월간 랭킹 dto에 createAt 추가

--- src/main/java/com/unsolvedwa/unsolvedwa/domain/ranking/dto/MonthRankingResponseDto.java
+++ src/main/java/com/unsolvedwa/unsolvedwa/domain/ranking/dto/MonthRankingResponseDto.java
@@ -1,6 +1,7 @@
 package com.unsolvedwa.unsolvedwa.domain.ranking.dto;
 
 import com.querydsl.core.annotations.QueryProjection;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,11 +11,13 @@ public class MonthRankingResponseDto {
   private String teamName;
   private String bojId;
   private Long score;
+  private LocalDateTime createAt;
 
   @QueryProjection
-  public MonthRankingResponseDto(String teamName, String bojId, Long score) {
+  public MonthRankingResponseDto(String teamName, String bojId, Long score, LocalDateTime createAt) {
     this.teamName = teamName;
     this.bojId = bojId;
     this.score = score;
+    this.createAt = createAt;
   }
 }
